77c7e754bd7f5391c1f85b8198ae7f456fae97c7
Ayspeed <chebbi41.bilel@hotmail.fr>
2023-09-19 11:55:00 +0200

    first commit

---
+++ _includes/script.js
@@ -0,0 +1,14 @@
+// Code JavaScript ici (si nécessaire)
+
+// Exemple : Ajoutez une interaction simple
+document.addEventListener("DOMContentLoaded", function () {
+    // Sélectionnez un élément
+    const myElement = document.querySelector("#accueil");
+
+    // Ajoutez un événement de clic
+    myElement.addEventListener("click", function () {
+        alert("Vous avez cliqué sur la section d'accueil !");
+    });
+    
+    // Vous pouvez ajouter plus de fonctionnalités ici en fonction de vos besoins.
+});
---
+++ index.md
@@ -0,0 +1,7 @@
+---
+layout: default  # Le modèle par défaut contiendra votre en-tête et pied de page
+title: Accueil   # Le titre de la page
+---
+
+# content
+<!-- Ajoutez ici le contenu spécifique de votre page d'accueil -->
